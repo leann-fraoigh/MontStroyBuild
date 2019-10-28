864e01de61460ea49f4802b0dd07d8dc4ef778f0
Leann Fraoigh <d.myshlennikova@gmail.com>
2019-10-28 16:21:24 +0100

    Commit

--- js/script.js
+++ js/script.js
@@ -124,6 +124,35 @@
   moveAdvantages();
 })();
 
+// // ПОЛИФИЛЛ ДЛЯ REMOVE в IE
+// (function () {
+//   var arr = [window.Element, window.CharacterData, window.DocumentType];
+//   var args = [];
+
+//   arr.forEach(function (item) {
+//     if (item) {
+//       args.push(item.prototype);
+//     }
+//   });
+
+//   // from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+//   (function (arr) {
+//     arr.forEach(function (item) {
+//       if (item.hasOwnProperty('remove')) {
+//         return;
+//       }
+//       Object.defineProperty(item, 'remove', {
+//         configurable: true,
+//         enumerable: true,
+//         writable: true,
+//         value: function remove() {
+//           this.parentNode.removeChild(this);
+//         }
+//       });
+//     });
+//   })(args);
+// })();
+
 // ПЕРЕМЕЩЕНИЕ БЛОКА ОПИСАНИЯ
 (function () {
   var moveDescription = function () {
@@ -155,7 +184,8 @@
         descriptionElem.classList.add('main-header__description');
         // Удаляем замену
         if (document.querySelector('.main-header__description-substitute')) {
-          document.querySelector('.main-header__description-substitute').remove();
+          // document.querySelector('.main-header__description-substitute').remove();
+          document.querySelector('.main-header__description-substitute').parentNode.removeChild(document.querySelector('.main-header__description-substitute'));
         }
       }
     }
@@ -192,3 +222,4 @@
     servicesLink.addEventListener('click', handleAnchorClick);
   }
 })();
+
